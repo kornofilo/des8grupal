e77cdc1a42b2f300c34b839fed9093d07de3634b
kevineliott <eliott23@outlook.es>
2017-10-26 23:54:11 -0500

    creacion de un nuevo from y utilizacion del subprog ShowDataGrid

---
+++ PROYECTO_GRUPOS/Proveedores.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Proveedores
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dataprovee = New System.Windows.Forms.DataGridView()
+        CType(Me.dataprovee, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dataprovee
+        '
+        Me.dataprovee.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dataprovee.Location = New System.Drawing.Point(42, 152)
+        Me.dataprovee.Name = "dataprovee"
+        Me.dataprovee.Size = New System.Drawing.Size(699, 307)
+        Me.dataprovee.TabIndex = 0
+        '
+        'Proveedores
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(778, 483)
+        Me.Controls.Add(Me.dataprovee)
+        Me.Name = "Proveedores"
+        Me.Text = "Proveedores"
+        CType(Me.dataprovee, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents dataprovee As System.Windows.Forms.DataGridView
+End Class
---
+++ PROYECTO_GRUPOS/Proveedores.vb
@@ -0,0 +1,10 @@
+﻿Public Class Proveedores
+
+    Private Sub Proveedores_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim estadoConexionDB As Boolean
+
+        estadoConexionDB = DBConnection.connect("cuentasxpagar")
+        
+        DBConnection.ShowDataGrid("proveedores", dataprovee)
+    End Sub
+End Class
