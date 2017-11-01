0f48711ee2aeae386869b3abaf0fb60baa124795
Logan8192 <wsses11000@gmail.com>
2017-10-31 20:41:41 -0500

    Tabla de compras actualizada

---
+++ Base de Datos/compras.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 01, 2017 at 02:39 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cuentasxpagar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `compras`
+--
+
+CREATE TABLE `compras` (
+  `idCompra` int(7) NOT NULL,
+  `idProveedor` int(7) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `idProducto` int(7) NOT NULL,
+  `cantidad` int(4) NOT NULL,
+  `costoUnitario` decimal(15,2) NOT NULL,
+  `totalCompra` decimal(15,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
